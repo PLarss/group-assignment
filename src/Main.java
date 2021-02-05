9a975b16501ebb59dcb6803d3ce525ccefefa4d8
PLarss <per.larsson@hm.com>
2021-02-05 13:49:38 +0100

    Created multiply method

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Test Multiply 5*5" + multiply(5,5) );
+    }
+
+    public static int multiply(int i, int x){
+
+        return i * x;
     }
 }
