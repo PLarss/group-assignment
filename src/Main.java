2ecba6d8de7da8f39ac98fc6b8acbac80a5a8f8d
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-02-05 13:50:23 +0100

    added subtract method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i = 10;
+        int y = 5;
+
+        int subtract = subtract(i, y);
+        System.out.println("Subtract result: " +subtract);
+    }
+
+    public static int subtract(int i, int y) {
+        return i-y;
     }
 }
